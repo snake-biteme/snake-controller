2d66a69a95218e56fb09b244969b59423e2893f4
Tina <tina.zhou.hui@gmail.com>
2022-04-21 15:52:47 +0100

    feat: random color generator

--- src/CONSTS.ts
+++ src/CONSTS.ts
@@ -3,4 +3,5 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const screenId = 'asdfsdfasdfsd';
 export const playerId = uuidv4();
-export const defColor = '#ff69b4';
+// random color generator https://stackoverflow.com/a/5092846/18631517
+export const defColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
