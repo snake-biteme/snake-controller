6586de8e9bad487e11983a22df2136e7c6d3c341
Tina <tina.zhou.hui@gmail.com>
2022-04-21 16:48:35 +0100

    feat: controllers styling

--- src/components/controllers/Controllers.tsx
+++ src/components/controllers/Controllers.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {motionHandler} from '../../services/apiClientAppSync';
 import {playerId, screenId} from '../../CONSTS';
+import styles from './Controllers.module.css';
 
 interface IProps {
     name: string,
@@ -19,12 +20,12 @@ function Controllers({name, color}: IProps) {
 
     return (
         <div>
-            <button onClick={() => handleClick(UP)}>Up</button>
+            <button className={styles.direction} onClick={() => handleClick(UP)}>Up</button>
             <div className="leftRight">
-                <button onClick={() => handleClick(LEFT)}>Left</button>
-                <button onClick={() => handleClick(RIGHT)}>Right</button>
+                <button className={`${styles.direction} ${styles.spaceRight}`} onClick={() => handleClick(LEFT)}>Left</button>
+                <button className={styles.direction} onClick={() => handleClick(RIGHT)}>Right</button>
             </div>
-            <button onClick={() => handleClick(DOWN)}>Down</button>
+            <button className={styles.direction} onClick={() => handleClick(DOWN)}>Down</button>
         </div>
     );
 }
