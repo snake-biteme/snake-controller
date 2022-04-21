826d760eb647cd9e70d8ee7a48307437dbd4c26d
Tina <tina.zhou.hui@gmail.com>
2022-04-21 16:24:14 +0100

    feat: initial screen styling

--- src/components/initialScreen/InitialScreen.tsx
+++ src/components/initialScreen/InitialScreen.tsx
@@ -1,6 +1,7 @@
 import React, { Dispatch, SetStateAction} from 'react';
 import {motionHandler} from '../../services/apiClientAppSync';
 import {playerId, screenId} from '../../CONSTS';
+import styles from './InitialScreen.module.css';
 
 interface IProps {
     // setState hook types: https://stackoverflow.com/a/56028976/18631517
@@ -22,10 +23,10 @@ function InitialScreen({showControls, setName, name, setColor, color}: IProps) {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input name="name" type="text" value={name} placeholder="Enter your name..." onChange={(event) => setName(event.target.value)}/>
-            <input name="color" type="color" value={color} placeholder="Choose your color..." onChange={(event) => setColor(event.target.value)}/>
-            <button type="submit">Start</button>
+        <form className={styles.startForm} onSubmit={handleSubmit}>
+            <input className={`${styles.input} ${styles.name}`} name="name" type="text" value={name} placeholder="Enter your name..." onChange={(event) => setName(event.target.value)}/>
+            <input className={`${styles.input} ${styles.color}`} name="color" type="color" value={color} placeholder="Choose your color..." onChange={(event) => setColor(event.target.value)}/>
+            <button className={`${styles.input} ${styles.submitButton}`} type="submit">Start</button>
         </form>
     );
 }
