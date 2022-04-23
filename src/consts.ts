772db2ae584d608cc02fc39de3fc2e478de8d76f
Tina <tina.zhou.hui@gmail.com>
2022-04-23 16:34:07 +0100

    feat: get screenId from url

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,12 @@
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 // https://bobbyhadz.com/blog/typescript-could-not-find-a-declaration-file-for-module-uuid
 
-export const screenId = 'asdfsdfasdfsd';
+const url = new URL(window.location.href);
+const params = new URLSearchParams(url.search);
+export const screenId = params.get('screenId') || '';
 export const playerId = uuidv4();
 // random color generator https://stackoverflow.com/a/5092846/18631517
-export const defColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+export const defColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
 
 // check for template variables
 const rootElement = document.getElementById('root');
