88545b198ca46290eae522635d52ec8cf3438bf0
Tina <tina.zhou.hui@gmail.com>
2022-05-01 18:00:36 +0100

    feat: controller arrows

--- src/components/controllers/VerticalControllers/VerticalControllers.tsx
+++ src/components/controllers/VerticalControllers/VerticalControllers.tsx
@@ -14,13 +14,12 @@ function VerticalControllers({handleClick, customSize}: IProps) {
 
     return (
         <div>
-            <button className={`${styles.direction} ${styles.up}`} onClick={() => handleClick(UP)} style={customSize}>Up</button>
+            <button className={`${styles.direction} ${styles.up}`} onClick={() => handleClick(UP)} style={customSize}/>
             <div className="leftRight">
-                <button className={`${styles.direction} ${styles.spaceRight}`} onClick={() => handleClick(LEFT)} style={customSize}>Left
-                </button>
-                <button className={styles.direction} onClick={() => handleClick(RIGHT)} style={customSize}>Right</button>
+                <button className={`${styles.direction} ${styles.left} ${styles.spaceRight}`} onClick={() => handleClick(LEFT)} style={customSize}/>
+                <button className={`${styles.direction} ${styles.right}`} onClick={() => handleClick(RIGHT)} style={customSize}/>
             </div>
-            <button className={`${styles.direction} ${styles.down}`} onClick={() => handleClick(DOWN)} style={customSize}>Down</button>
+            <button className={`${styles.direction} ${styles.down}`} onClick={() => handleClick(DOWN)} style={customSize}/>
         </div>
     );
 }
